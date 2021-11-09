41eb1b405aa78d312ca1ef8c605c8f4ac63dbfd1
DESKTOP-FLR7CTO\sleep <yuwike.19009@mhs.unesa.ac.id>
2021-11-09 21:04:03 +0700

    Edit profile

---
+++ resources/views/editprofile.blade.php
@@ -0,0 +1,39 @@
+div class="container rounded bg-white mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-3 border-right">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Loremipsum</span><span class="text-black-50">loremipsum@gmail.com.my</span><span> </span></div>
+        </div>
+        <div class="col-md-5 border-right">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Edit Profile</h4>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6"><label class="labels">Nama Depan</label><input type="text" class="form-control" placeholder="nama depan" value=""></div>
+                    <div class="col-md-6"><label class="labels">Nama Belakang</label><input type="text" class="form-control" value="" placeholder="nama belakang"></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12"><label class="labels">Nomor Telepon</label><input type="text" class="form-control" placeholder="masukkan nomor telepon" value=""></div>
+                    <div class="col-md-12"><label class="labels">Alamat</label><input type="text" class="form-control" placeholder="masukkan alamat" value=""></div>
+                    <div class="col-md-12"><label class="labels">Kode Pos</label><input type="text" class="form-control" placeholder="kode pos" value=""></div>
+                    <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="masukkan email" value=""></div>
+                    <div class="col-md-12"><label class="labels">Keahlian</label><input type="text" class="form-control" placeholder="keahlian" value=""></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-6"><label class="labels">Provinsi</label><input type="text" class="form-control" placeholder="provinsi" value=""></div>
+                    <div class="col-md-6"><label class="labels">Kota</label><input type="text" class="form-control" value="" placeholder="kota"></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center experience"><span>Edit Pengalaman</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Pengalaman</span></div><br>
+                <div class="col-md-12"><label class="labels">Pengalaman</label><input type="text" class="form-control" placeholder="pengalaman" value=""></div> <br>
+                <div class="col-md-12"><label class="labels">Detail pengalaman</label><input type="text" class="form-control" placeholder="detail tambahan" value=""></div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
