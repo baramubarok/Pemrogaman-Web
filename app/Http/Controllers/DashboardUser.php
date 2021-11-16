1a22c4546cf6c5fb94f443e03299d9156c132c21
baramubarok <bara.mubarok9110@gmail.com>
2021-11-17 01:49:51 +0700

    dashboard done

--- app/Http/Controllers/DashboardUser.php
+++ app/Http/Controllers/DashboardUser.php
@@ -16,12 +16,47 @@ class DashboardUser extends Controller
         $user_profile = UserProfile::where('user_id', '=', Auth::user()->id)->first();
         $user_order = Order::where('user_id', '=', Auth::user()->id)->get();
         $shop = Provider::where('user_id', '=', Auth::user()->id)->first();
-        return view('user.index', compact(['user_profile', 'user_order', 'shop']));
-    }
-
-    public function order()
-    {
-        return view('user.order');
+        $countOrder = Order::where([
+            ['user_id', '=', Auth::user()->id],
+            ['status', '=', 0]
+        ])->count();
+        $countPayment = Order::where([
+            ['user_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_method', '=', 1],
+            ['payment_status', '=', 0]
+        ])->count();
+        $countProcess = Order::where([
+            ['user_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_status', '=', 1]
+        ])->count();
+        $countProcess2 = Order::where([
+            ['user_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_method', '=', 0],
+            ['payment_status', '=', 0]
+        ])->count();
+        $countProcess = $countProcess + $countProcess2;
+        $countComplete = Order::where([
+            ['user_id', '=', Auth::user()->id],
+            ['status', '=', 2]
+        ])->count();
+        $order = Order::where('user_id', '=', Auth::user()->id)->get();
+        foreach ($order as $o) {
+            $user = Provider::where('id', '=', $o->provider_id)->first();
+            $o->user = $user;
+        }
+        return view('user.index', compact([
+            'user_profile',
+            'user_order',
+            'shop',
+            'countOrder',
+            'countPayment',
+            'countProcess',
+            'countComplete',
+            'order',
+        ]));
     }
 
     public function updateProfile(Request $request)
@@ -34,13 +69,15 @@ class DashboardUser extends Controller
             'telp' => $request->telp,
             'address' => $request->address,
         ]);
-        if ($request->hasFile('photo')) {
-            $photo = $request->photo;
-            $filename = Auth::user()->id . rand() . '.' . $photo->getClientOriginalExtension();
-            $photo->move('data/images/user/profile', $filename);
-            $user_profile->update([
-                'photo' => $filename,
-            ]);
+        if ($request->photo != null) {
+            if ($request->hasFile('photo')) {
+                $p = $request->photo;
+                $filename = Auth::user()->id . rand() . '.' . $p->getClientOriginalExtension();
+                $p->move('data/images/user/profile', $filename);
+                $user_profile->update([
+                    'photo' => $filename,
+                ]);
+            }
         }
         $user_shop->update([
             'identity_type' => $request->identity_type,
@@ -49,11 +86,11 @@ class DashboardUser extends Controller
         if ($request->hasFile('identity_photo') && $request->hasFile('identity_selfie_photo')) {
 
             $photo1 = $request->identity_photo;
-            $filename1 = Auth::user()->id . 1 . rand() . '.' . $photo->getClientOriginalExtension();
+            $filename1 = Auth::user()->id . 1 . rand() . '.' . $photo1->getClientOriginalExtension();
             $photo1->move('data/images/user/validation', $filename1);
 
             $photo2 = $request->identity_selfie_photo;
-            $filename2 = Auth::user()->id . 2 . rand() . '.' . $photo->getClientOriginalExtension();
+            $filename2 = Auth::user()->id . 2 . rand() . '.' . $photo2->getClientOriginalExtension();
             $photo2->move('data/images/user/validation', $filename2);
 
             $user_shop->update([
@@ -61,6 +98,15 @@ class DashboardUser extends Controller
                 'identity_selfie_photo' => $filename2,
             ]);
         }
+        return redirect()->back()->with(['success' => 'Data Berhasil Diupdate']);
+    }
+
+    public function updateOrder(Request $request)
+    {
+        $shop = Order::where('id', '=', $request->id)->first();
+        $shop->update([
+            'status' => $request->status
+        ]);
         return redirect()->back()->with(['success' => 'Data Berhasil Diupdate']);
     }
 }
--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -2,13 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Provider;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProviderController extends Controller
 {
     //
     public function index()
     {
-        dd('tes');
+        // dd('tes');
+        $user_profile = UserProfile::where('user_id', '=', Auth::user()->id)->first();
+        $shop = Provider::where('user_id', '=', Auth::user()->id)->first();
+        $countOrder = Order::where([
+            ['provider_id', '=', Auth::user()->id],
+            ['status', '=', 0]
+        ])->count();
+        $countPayment = Order::where([
+            ['provider_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_method', '=', 1],
+            ['payment_status', '=', 0]
+        ])->count();
+        $countProcess = Order::where([
+            ['provider_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_status', '=', 1]
+        ])->count();
+        $countProcess2 = Order::where([
+            ['provider_id', '=', Auth::user()->id],
+            ['status', '=', 1],
+            ['payment_method', '=', 0],
+            ['payment_status', '=', 0]
+        ])->count();
+        $countProcess = $countProcess + $countProcess2;
+        $countComplete = Order::where([
+            ['provider_id', '=', Auth::user()->id],
+            ['status', '=', 2]
+        ])->count();
+        $order = Order::where('provider_id', '=', Auth::user()->id)->get();
+        foreach ($order as $o) {
+            $user = UserProfile::where('user_id', '=', $o->user_id)->first();
+            $o->user = $user;
+        }
+
+        return view('shop.profile', compact([
+            'shop',
+            'user_profile',
+            'countOrder',
+            'countPayment',
+            'countProcess',
+            'countComplete',
+            'order'
+        ]));
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $shop = Provider::where('user_id', '=', Auth::user()->id)->first();
+        $shop->update([
+            'name' => $request->name,
+            'category' => $request->category,
+            'service' => $request->service,
+            'description' => $request->desc,
+            'location' => $request->address,
+            'maps_link' => $request->maps_link,
+            'service_area' => $request->service_area,
+            'price' => $request->price,
+        ]);
+        return redirect()->back()->with(['success' => 'Data Berhasil Diupdate']);
+    }
+
+    public function updateStatus(Request $request)
+    {
+        $shop = Provider::where('user_id', '=', Auth::user()->id)->first();
+        if ($request->publish == 'on') {
+            $shop->update([
+                'status' => 1
+            ]);
+        } else {
+            $shop->update([
+                'status' => 0
+            ]);
+        }
+        return redirect()->back()->with(['success' => 'Data Berhasil Diupdate']);
+    }
+
+    public function updateOrder(Request $request)
+    {
+        $shop = Order::where('id', '=', $request->id)->first();
+        $shop->update([
+            'status' => $request->status
+        ]);
+        return redirect()->back()->with(['success' => 'Data Berhasil Diupdate']);
     }
 }
